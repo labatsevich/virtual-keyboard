9fbe45c0f30ffcab8c19b7a82d4ce196b5f6651b
yurilabatsevich <yuralabatsevich@gmail.com>
2022-05-04 22:58:06 +0300

    refactor: keymaps and keyboard class

--- keyboard/src/keyboard.js
+++ keyboard/src/keyboard.js
@@ -8,6 +8,9 @@ class Keyboard {
         this.keys = [
             ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Minus', 'Equal', 'Backspace'],
             ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight'],
+            ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+            ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight'],
+            ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'MetaRight', 'ContextMenu', 'ControlRight', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'],
         ];
     }
 
@@ -18,9 +21,9 @@ class Keyboard {
 
         this.keys.forEach((row) => {
             const buttons = row.map((item) => {
-                const { code, key, shiftKey } = collection.find((k) => k.code === item) || {};
+                const { code, lower, upper } = collection.find((k) => k.code === item) || {};
 
-                const button = createControl('button', key, ['keyboard__key', code]);
+                const button = createControl('button', lower, ['keyboard__key', code.toLowerCase()]);
 
                 return button;
             });
--- keyboard/src/utils/keysMap.js
+++ keyboard/src/utils/keysMap.js
@@ -1,78 +1,344 @@
+/* eslint-disable indent */
 export default [
 
     {
         lang: 'en',
         keys: [{
                 code: 'Backquote',
-                key: '`',
-                shiftKey: '~',
+                lower: '`',
+                upper: '~',
             },
             {
                 code: 'Digit1',
-                key: '1',
-                shiftKey: '!',
+                lower: '1',
+                upper: '!',
             },
             {
                 code: 'Digit2',
-                key: '2',
-                shiftKey: '@',
+                lower: '2',
+                upper: '@',
             },
             {
                 code: 'Digit3',
-                key: '3',
-                shiftKey: '#',
+                lower: '3',
+                upper: '#',
             },
             {
                 code: 'Digit4',
-                key: '4',
-                shiftKey: '$',
+                lower: '4',
+                upper: '$',
             },
             {
                 code: 'Digit5',
-                key: '5',
-                shiftKey: '%',
+                lower: '5',
+                upper: '%',
             },
             {
                 code: 'Digit6',
-                key: '6',
-                shiftKey: '^',
+                lower: '6',
+                upper: '^',
             },
             {
                 code: 'Digit7',
-                key: '7',
-                shiftKey: '&',
+                lower: '7',
+                upper: '&',
             },
             {
                 code: 'Digit8',
-                key: '8',
-                shiftKey: '*',
+                lower: '8',
+                upper: '*',
             },
             {
                 code: 'Digit9',
-                key: '9',
-                shiftKey: '(',
+                lower: '9',
+                upper: '(',
             },
             {
                 code: 'Digit0',
-                key: '0',
-                shiftKey: ')',
+                lower: '0',
+                upper: ')',
             },
             {
                 code: 'Minus',
-                key: '-',
-                shiftKey: '_',
+                lower: '-',
+                upper: '_',
             },
             {
                 code: 'Equal',
-                key: '=',
-                shiftKey: '+',
+                lower: '=',
+                upper: '+',
             },
             {
                 code: 'Backspace',
-                key: 'Backspace',
-                shiftKey: 'Backspace',
+                lower: 'Backspace',
+                upper: 'Backspace',
+            },
+            {
+                code: 'Tab',
+                lower: 'Tab',
+                upper: 'Tab',
+            },
+            {
+                code: 'KeyQ',
+                lower: 'q',
+                upper: 'Q',
+            },
+            {
+                code: 'KeyW',
+                lower: 'w',
+                upper: 'W',
+            },
+            {
+                code: 'KeyE',
+                lower: 'e',
+                upper: 'E',
+            },
+            {
+                code: 'KeyR',
+                lower: 'r',
+                upper: 'R',
+            },
+            {
+                code: 'KeyT',
+                lower: 't',
+                upper: 'T',
+            },
+            {
+                code: 'KeyY',
+                lower: 'y',
+                upper: 'Y',
+            },
+            {
+                code: 'KeyU',
+                lower: 'u',
+                upper: 'U',
+            },
+            {
+                code: 'KeyI',
+                lower: 'i',
+                upper: 'I',
+            },
+            {
+                code: 'KeyO',
+                lower: 'o',
+                upper: 'O',
+            },
+            {
+                code: 'KeyP',
+                lower: 'p',
+                upper: 'P',
+            },
+            {
+                code: 'BracketLeft',
+                lower: '[',
+                upper: '{',
+            },
+            {
+                code: 'BracketRight',
+                lower: ']',
+                upper: '}',
+            },
+            {
+                code: 'BracketLeft',
+                lower: '[',
+                upper: '{',
+            },
+            {
+                code: 'CapsLock',
+                lower: 'CapsLock',
+                upper: 'CapsLock',
+            },
+            {
+                code: 'KeyA',
+                lower: 'a',
+                upper: 'A',
+            },
+            {
+                code: 'KeyS',
+                lower: 's',
+                upper: 'S',
+            },
+            {
+                code: 'KeyD',
+                lower: 'd',
+                upper: 'D',
+            },
+            {
+                code: 'KeyF',
+                lower: 'f',
+                upper: 'F',
+            },
+            {
+                code: 'KeyG',
+                lower: 'g',
+                upper: 'G',
+            },
+            {
+                code: 'KeyH',
+                lower: 'h',
+                upper: 'H',
+            },
+            {
+                code: 'KeyJ',
+                lower: 'j',
+                upper: 'J',
+            },
+            {
+                code: 'KeyK',
+                lower: 'k',
+                upper: 'K',
+            },
+            {
+                code: 'KeyL',
+                lower: 'l',
+                upper: 'L',
+            },
+            {
+                code: 'Semicolon',
+                lower: ';',
+                upper: ':',
+            },
+            {
+                code: 'Quote',
+                lower: '\'',
+                upper: '"',
+            },
+            {
+                code: 'Semicolon',
+                lower: ';',
+                upper: ':',
+            },
+            {
+                code: 'Enter',
+                lower: 'Enter',
+                upper: 'Enter',
+            },
+            {
+                code: 'ShiftLeft',
+                lower: 'Shift',
+                upper: 'Shift',
+            },
+            {
+                code: 'IntlBackslash',
+                lower: '\\',
+                upper: '|',
+            },
+            {
+                code: 'KeyZ',
+                lower: 'z',
+                upper: 'Z',
+            },
+            {
+                code: 'KeyX',
+                lower: 'x',
+                upper: 'X',
+            },
+            {
+                code: 'KeyC',
+                lower: 'c',
+                upper: 'C',
+            },
+            {
+                code: 'KeyV',
+                lower: 'v',
+                upper: 'V',
+            },
+            {
+                code: 'KeyB',
+                lower: 'b',
+                upper: 'B',
+            },
+            {
+                code: 'KeyN',
+                lower: 'n',
+                upper: 'N',
+            },
+            {
+                code: 'KeyM',
+                lower: 'm',
+                upper: 'M',
+            },
+            {
+                code: 'Comma',
+                lower: ',',
+                upper: '<',
+            },
+            {
+                code: 'Period',
+                lower: '.',
+                upper: '>',
+            },
+            {
+                code: 'Slash',
+                lower: '/',
+                upper: '?',
+            },
+            {
+                code: 'ShiftRight',
+                lower: 'Shift',
+                upper: 'Shift',
+            },
+            {
+                code: 'ControlLeft',
+                lower: 'Ctrl',
+                upper: 'Ctrl',
+            },
+            {
+                code: 'MetaLeft',
+                lower: 'Win',
+                upper: 'Win',
+            },
+            {
+                code: 'AltLeft',
+                lower: 'Alt',
+                upper: 'Alt',
+            },
+            {
+                code: 'Space',
+                lower: 'Space',
+                upper: 'Space',
+            },
+            {
+                code: 'AltRight',
+                lower: 'Alt',
+                upper: 'Alt',
+            },
+            {
+                code: 'MetaRight',
+                lower: 'Win',
+                upper: 'Win',
+            },
+            {
+                code: 'ContextMenu',
+                lower: 'Menu',
+                upper: 'Menu',
             },
 
+            {
+                code: 'ControlRight',
+                lower: 'Ctrl',
+                upper: 'Ctrl',
+            },
+            {
+                code: 'ArrowLeft',
+                lower: '🠔',
+                upper: '🠔',
+            },
+            {
+                code: 'ArrowRight',
+                lower: '🠖',
+                upper: '🠖',
+            },
+            {
+                code: 'ArrowUp',
+                lower: '🠕',
+                upper: '🠕',
+            },
+            {
+                code: 'ArrowDown',
+                lower: '🠗',
+                upper: '🠗',
+            },
         ],
     },
 
