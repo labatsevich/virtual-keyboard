2bbdb595fc9ed671e4910e10bedb75c9f0a7399e
yurilabatsevich <yurilabatsevich@gmail.com>
2022-05-09 01:09:52 +0300

    feat: add description with keyboard shortcut for changing language

--- keyboard/src/app.js
+++ keyboard/src/app.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/extensions */
 import Keyboard from './keyboard.js';
+import createControl from './utils/helpers.js';
 
 const lang = localStorage.getItem('lang');
 const keyboard = new Keyboard(lang);
@@ -10,3 +11,6 @@ document.addEventListener('click', (event) => keyboard.eventHandler(event));
 document.addEventListener('keydown', (event) => keyboard.eventHandler(event));
 document.addEventListener('keyup', (event) => keyboard.eventHandler(event));
 document.addEventListener('mouseup', (event) => keyboard.eventHandler(event));
+
+document.body.insertBefore(createControl('div', 'Клавиатура создана в операционной системе Windows', ['keyboard__info']), document.querySelector('.keyboard'));
+document.body.insertBefore(createControl('div', 'Для переключения языка комбинация: левыe ctrl + alt', ['keyboard__info']), document.querySelector('.keyboard__info'));
