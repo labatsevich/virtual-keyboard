7bac332abdc6987bca218560c90c1cd47949932e
yurilabatsevich <yurilabatsevich@gmail.com>
2022-05-08 23:36:12 +0300

    refactor: refactor createElement function

--- keyboard/src/utils/helpers.js
+++ keyboard/src/utils/helpers.js
@@ -1,7 +1,9 @@
-export default function createControl(tag, text, classes) {
+const createControl = (tag, text, classes = []) => {
     const control = document.createElement(tag);
     control.classList.add(...classes);
     control.innerText = text;
 
     return control;
-}
+};
+
+export default createControl;
