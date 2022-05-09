04c9992b859e0062db31e63d6f8856a4360c80a7
yurilabatsevich <yurilabatsevich@gmail.com>
2022-05-09 20:28:39 +0300

    feat: add feature to indicate current language

--- keyboard/src/app.js
+++ keyboard/src/app.js
@@ -14,3 +14,6 @@ document.addEventListener('mouseup', (event) => keyboard.eventHandler(event));
 
 document.body.insertBefore(createControl('div', 'Клавиатура создана в операционной системе Windows', ['keyboard__info']), document.querySelector('.keyboard'));
 document.body.insertBefore(createControl('div', 'Для переключения языка комбинация: левыe ctrl + alt', ['keyboard__info']), document.querySelector('.keyboard__info'));
+document.body.insertBefore(createControl('div', 'Язык клавиатуры:', ['lang__info']), document.querySelector('.keyboard__info'));
+const span = createControl('span', lang, ['lang__indicator']);
+document.querySelector('.lang__info').append(span);
